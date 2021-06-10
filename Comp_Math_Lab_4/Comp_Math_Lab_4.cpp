9616492e1885572207405bca2adebbdcc0864c1b
Timama <76005360+Timamazzz@users.noreply.github.com>
2021-06-10 23:42:39 +0300

    Add files via upload

---
+++ Comp_Math_Lab_4/Comp_Math_Lab_4.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+int n = 4; double** deltaY, * x, * f;
+double getNewtonSequence(double h, double xref) {
+	double t = (xref - x[0]) / h;
+	double sum = f[0];
+	for (int i = 0; i < n - 1; i++) {
+		double coef = t;
+		for (int j = 0; j < i; j++)
+			coef = coef * (t - (j + 1)) / (j + 2);
+		sum += coef * deltaY[i][0];
+	}
+	return sum;
+}
+
+int frontRoute(double* a) {
+	double h = x[1] - x[0];
+	for (int i = 2; i < n; i++) { if (x[i] - x[i - 1] != h) return -1; }
+	// Считаем разности функции
+	deltaY = new double* [n - 1];
+	for (int i = 0; i < n; i++) deltaY[i] = new double[n - 1 - i];
+	// Порядок разностей функций для массива deltaY[i][j] равен i + 1
+	// Считаем первые разности функций
+	for (int i = 0; i < n - 1; i++) deltaY[0][i] = f[i + 1] - f[i];
+	// Считаем следующие порядки
+	for (int i = 1; i < n - 1; i++) {
+		for (int j = 0; j < n - 1 - i; j++) deltaY[i][j] = deltaY[i - 1][j + 1] - deltaY[i - 1][j];
+	}
+	cout << "Разности функций:\n";
+	for (int i = 0; i < n - 1; i++) {
+		cout << i + 1 << ": ";
+		for (int j = 0; j < n - 1 - i; j++) cout << "deltaY[" << j << "] = " << deltaY[i][j] << "; ";
+		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) cout << "Значение f(x) в точке a[" << i + 1 << "] = " << a[i] << " равно " << getNewtonSequence(h, a[i]) << endl;
+	return 0;
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+	cout << "Введите n: "; cin >> n;
+	x = new double[n]; f = new double[n];
+	for (int i = 0; i < n; i++) {
+		cout << "x[" << i << "]: "; cin >> x[i];
+		cout << "f(x[" << i << "]): "; cin >> f[i];
+	}
+
+	double a[3]; a[0] = (x[0] + x[1]) / 2; a[1] = (x[1] + x[2]) / 2; a[2] = (x[2] + x[3]) / 2;
+	if (frontRoute(a) == -1) cout << "Невозможно посчитать h, разница элементов не статична!\n";
+	system("pause");
+	return 0;
+}
---
+++ Comp_Math_Lab_5/Comp_Math_Lab_5.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+using namespace std;
+void Work(double* X, double* Y, int count, double step)
+{
+	double* firstDerivative = new double[count], * secondDerivative = new double[count];
+	firstDerivative[0] = (Y[1] - Y[0]) / step;
+	firstDerivative[count - 1] = (Y[count - 1] - Y[count - 2]) / step;
+	for (int i = 1; i != count - 1; ++i)
+		firstDerivative[i] = (Y[i + 1] - Y[i - 1]) / (2 * step);
+	secondDerivative[0] = (firstDerivative[1] - firstDerivative[0]) / step;
+	secondDerivative[count - 1] = (firstDerivative[count - 1] - firstDerivative[count - 2]) / step;
+	for (int i = 1; i != count - 1; ++i)
+		secondDerivative[i] = (Y[i + 1] - 2 * Y[i] + Y[i - 1]) / pow(step, 2);
+	cout << endl << "Результат:" << endl << endl;
+	for (int i = 0; i != count; ++i)
+	{
+		cout << "y'(" << X[i] << ") = " << firstDerivative[i] << endl;
+		cout << "y''(" << X[i] << ") = " << secondDerivative[i] << endl << endl;
+	}
+	delete[]firstDerivative;
+	delete[]secondDerivative;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int count = 0;
+	cout << "Введите количество узлов: ";
+	cin >> count;
+	double* X = new double[count], * Y = new double[count];
+	cout << "Введите значения X: ";
+	for (int i = 0; i != count; ++i)
+		cin >> X[i];
+	cout << "Введите значения Y: ";
+	for (int i = 0; i != count; ++i)
+		cin >> Y[i];
+	Work(X, Y, count, X[1] - X[0]);
+	delete[]X;
+	delete[]Y;
+	system("Pause");
+}
